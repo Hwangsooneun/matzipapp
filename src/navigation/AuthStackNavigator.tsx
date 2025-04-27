ea446e3de20a849bd108cf7be48964b0549513f1
hwangsooneun <catalyst8849@gmail.com>
2025-04-27 15:55:23 +0900

    Feat: stack navigation

---
+++ src/navigation/AuthStackNavigator.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import AuthHomeScreen from '../screens/AuthHomeScreen';
+import LoginScreen from '../screens/LoginScreen';
+import {authNavigations} from '../constants';
+export type AuthStackParamList = {
+  [authNavigations.AUTH_HOME]: undefined;
+  [authNavigations.LOGIN]: undefined;
+};
+
+function AuthStackNavigator() {
+  const Stack = createStackNavigator<AuthStackParamList>();
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name={authNavigations.AUTH_HOME}
+        component={AuthHomeScreen}
+      />
+      <Stack.Screen name={authNavigations.LOGIN} component={LoginScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default AuthStackNavigator;
