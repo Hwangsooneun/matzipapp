c82b1a7fa4e729a0b2d9b0f0a5f9dfa12574c068
hwangsooneun <catalyst8849@gmail.com>
2025-04-27 16:38:16 +0900

    Feat: 스크린 구조 및 옵션 설정

---
+++ src/screens/calender/CalenderHomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function CalenderHomeScreen() {
+  return (
+    <View>
+      <Text>캘린더</Text>
+    </View>
+  );
+}
+
+export default CalenderHomeScreen;
