3844f63dc724d754b3775a512161e2ecdd641c89
hwangsooneun <catalyst8849@gmail.com>
2025-04-28 17:18:10 +0900

    Feat: react query 도입, 로그인 회원가입 연동

---
+++ src/types/common.ts
@@ -0,0 +1,27 @@
+import {
+  QueryKey,
+  UseMutationOptions,
+  UseQueryOptions,
+} from '@tanstack/react-query';
+import {AxiosError} from 'axios';
+
+type ResponseError = AxiosError<{
+  statusCode: string;
+  message: string;
+  error: string;
+}>;
+
+type UseMutationCustomOptions<TData = unknown, TVariables = unknown> = Omit<
+  UseMutationOptions<TData, ResponseError, TVariables, unknown>,
+  'mutationFn'
+>;
+
+type UserQUeryCustomOptions<
+  TQueryFnData = unknown,
+  TData = TQueryFnData,
+> = Omit<
+  UseQueryOptions<TQueryFnData, ResponseError, TData, QueryKey>,
+  'queryKey'
+>;
+
+export type {ResponseError, UseMutationCustomOptions, UserQUeryCustomOptions};
