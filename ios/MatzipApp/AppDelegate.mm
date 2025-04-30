eb9870b7fde17fa9d4864d7b3b33d3565ad8e2bb
hwangsooneun <catalyst8849@gmail.com>
2025-04-30 15:39:14 +0900

    Feat: google api key 환경변수 설정

--- ios/MatzipApp/AppDelegate.mm
+++ ios/MatzipApp/AppDelegate.mm
@@ -1,13 +1,15 @@
 #import "AppDelegate.h"
 
 #import <GoogleMaps/GoogleMaps.h>
+#import "RNCConfig.h"
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [GMSServices provideAPIKey:@"xxx"];
+  NSString *apiKey = [RNCConfig envFor:@"GOOGLE_MAPS_API_KEY"];
+  [GMSServices provideAPIKey:apiKey];
   self.moduleName = @"MatzipApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
